2bea5143a701585641a5d3d87fd00e64cd18718a
DanielMuritiba <danielsmuritiba@gmail.com>
2025-05-05 20:11:39 -0300

    basic configs

---
+++ src/main/java/com/sha/translator_docs/TranslatorDocsApplication.java
@@ -0,0 +1,13 @@
+package com.sha.translator_docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslatorDocsApplication.class, args);
+	}
+
+}
